9b1e027956abf5072329aed0cb3c30bdd1ad321c
imjeen <imjeen@sina.com>
2020-09-14 21:24:30 +0800

    :sparkles: add the touch start to end to show origin image

--- src/comtainers/Effect/FilterSVG/Main/index.tsx
+++ src/comtainers/Effect/FilterSVG/Main/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, forwardRef, DetailedHTMLProps, LegacyRef, SVGAttributes } from 'react';
+import React, { useState, useContext, forwardRef, DetailedHTMLProps, LegacyRef, SVGAttributes } from 'react';
 import { useStoreState } from '@/store';
 import { FilterContext } from '../FilterContext';
 
@@ -26,9 +26,18 @@ function MainSvg(props: DetailedHTMLProps<SVGAttributes<SVGSVGElement>, SVGSVGEl
 
     const { width, height } = { width: 750, height: 1000 };
     const viewBoxValue = [0, 0, width, height];
+
+    const [hasOrigin, setHasOrigin] = useState(false);
+    const hanleStart = () => setHasOrigin(true);
+    const handleEnd = () => setHasOrigin(false);
     return (
         <>
             <svg
+                onTouchStart={hanleStart}
+                onTouchEnd={handleEnd}
+                onMouseDown={hanleStart}
+                onMouseUp={handleEnd}
+                onMouseLeave={handleEnd}
                 ref={ref}
                 className={styles.svg}
                 xmlns="http://www.w3.org/2000/svg"
@@ -41,10 +50,10 @@ function MainSvg(props: DetailedHTMLProps<SVGAttributes<SVGSVGElement>, SVGSVGEl
                 <defs>
                     {/* thumbnail filter */}
                     {palatteFilters.map(({ filterId, filter, playgrounds }) => (
-                        <Filter id={filterId} filter={filter} playgrounds={playgrounds} />
+                        <Filter key={filterId} id={filterId} filter={filter} playgrounds={playgrounds} />
                     ))}
                     {/* live filter */}
-                    <Filter id={id} filter={filter} playgrounds={playgrounds} />
+                    <Filter id={hasOrigin ? `${id}.` : id} filter={filter} playgrounds={playgrounds} />
                 </defs>
                 <g>
                     <image xlinkHref={base64} width="100%" height="100%" />
